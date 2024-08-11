f6125442464092f55c0f8712a377b93a2f48b24b
fellipetav <fellipe@bytesfritos.com.br>
2024-08-11 18:57:38 -0300

    feat: implementa o findById no SellerDaoJDBC

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,22 +1,23 @@
 package com.example;
 
-import java.util.Date;
+// import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
-import model.entities.Seller;
+// import model.entities.Department;
+// import model.entities.Seller;
 
 public final class App {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 1000.0, obj);
+        // Department obj = new Department(1, "Books");
+        // Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 1000.0, obj);
+
         SellerDao sellerDao = DaoFactory.createSellerDao(); 
         // Assim, meu programa não conhece a implementação, mas apenas a interface.
         // É também uma forma de injeção de independência sem explicitar a implementação.
 
-        System.out.println(seller);
+        System.out.println(sellerDao.findById(3));
     }
 }
--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -2,10 +2,24 @@ package model.dao.impl;
 
 import java.util.List;
 
+import db.DbException;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection connection;
+
+    public SellerDaoJDBC(Connection connection) {
+        this.connection = connection;
+    }
 
     @Override
     public void insert(Seller object) {
@@ -27,9 +41,37 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'findById'");
-        return null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE seller.Id = ?");
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Department dept = new Department(
+                        resultSet.getInt("DepartmentId"),
+                        resultSet.getString("DepName"));
+                Seller seller = new Seller(
+                        id,
+                        resultSet.getString("Name"),
+                        resultSet.getString("Email"),
+                        resultSet.getDate("BirthDate"),
+                        resultSet.getDouble("BaseSalary"),
+                        dept);
+                return seller;
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
