d6ee1ff1af0f339198ceb1594312247f5be8268f
fellipetav <fellipe@bytesfritos.com.br>
2024-08-11 13:04:37 -0300

    :tada: first commit: demo_dao_jdbc projeto de aula

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import model.entities.Department;
+
+public final class App {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+}
